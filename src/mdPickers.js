242c9a79f5821afe79caf602bd8ca1a8c392d5be
alenaksu <misterroot@tiscali.it>
2016-03-08 10:07:39 +0000

    Added svg registry

--- src/mdPickers.js
+++ src/mdPickers.js
@@ -2,4 +2,16 @@ var module = angular.module("mdPickers", [
 	"ngMaterial",
 	"ngAnimate",
 	"ngAria"
-]); 
+]);
+
+module.config(["$mdIconProvider", "mdpIconsRegistry", function($mdIconProvider, mdpIconsRegistry) {
+	angular.forEach(mdpIconsRegistry, function(icon, index) {
+		$mdIconProvider.icon(icon.id, icon.url);
+	});
+}]);
+
+module.run(["$templateCache", "mdpIconsRegistry", function($templateCache, mdpIconsRegistry) {
+	angular.forEach(mdpIconsRegistry, function(icon, index) {
+		$templateCache.put(icon.url, icon.svg);
+	});
+}]);
