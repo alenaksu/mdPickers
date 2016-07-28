b93b6a4563752f5744d863d9ab94d1376823b190
Divya <divya.mopuri@capstonec.com>
2016-07-28 14:38:40 -0500

    B-02964:Added lable attribute to mdp date and time pickers to replace placeholder as placeholder is having validation error messages issues in IE browser. Placeholder still exists to support the existing behavior, if both label and placeholder present labrel will get the priority

--- src/components/mdpTimePicker/mdpTimePicker.js
+++ src/components/mdpTimePicker/mdpTimePicker.js
@@ -248,14 +248,16 @@ module.directive("mdpTimePicker", ["$mdpTimePicker", "$timeout", function($mdpTi
         template: function(element, attrs) {
             var noFloat = angular.isDefined(attrs.mdpNoFloat),
                 placeholder = angular.isDefined(attrs.mdpPlaceholder) ? attrs.mdpPlaceholder : "",
-                openOnClick = angular.isDefined(attrs.mdpOpenOnClick) ? true : false;
-            
+                openOnClick = angular.isDefined(attrs.mdpOpenOnClick) ? true : false,
+                label = angular.isDefined(attrs.mdpLabel) ? attrs.mdpLabel : "";
+
             return '<div layout layout-align="start start">' +
-                    '<md-button class="md-icon-button" ng-click="showPicker($event)"' + (angular.isDefined(attrs.mdpDisabled) ? ' ng-disabled="disabled"' : '') + '>' +
+                    '<md-button class="md-icon-button" ng-click="showPicker($event)"' + (angular.isDefined(attrs.mdpDisabled) ? ' ng-disabled="disabled"' : '') + ' aria-label="clock">' +
                         '<md-icon md-svg-icon="mdp-access-time"></md-icon>' +
                     '</md-button>' +
                     '<md-input-container' + (noFloat ? ' md-no-float' : '') + ' md-is-error="isError()">' +
-                        '<input type="{{ ::type }}"' + (angular.isDefined(attrs.mdpDisabled) ? ' ng-disabled="disabled"' : '') + ' aria-label="' + placeholder + '" placeholder="' + placeholder + '"' + (openOnClick ? ' ng-click="showPicker($event)" ' : '') + ' />' +
+                    (label ? '<label>'+label+'</label>' : '')+
+                    '<input type="{{ ::type }}"' + (angular.isDefined(attrs.mdpDisabled) ? ' ng-disabled="disabled"' : '') + ' aria-label="' + (!label && placeholder ?placeholder : label) + '"' + (!label && placeholder ? ' placeholder="' + placeholder+'"' : '') + (openOnClick ? ' ng-click="showPicker($event)" ' : '') + ' />' +
                     '</md-input-container>' +
                 '</div>';
         },
