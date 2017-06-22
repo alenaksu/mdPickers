36f3235a2520ae52be4fe4c2660605b275254e22
Robin THONI <robin@rthoni.com>
2017-06-21 23:28:48 -0500

    Updated demo

--- demo/demoController.js
+++ demo/demoController.js
@@ -4,4 +4,15 @@
 
 angular.module('demo').controller('demoController', ['$scope', function($scope) {
     $scope.date1 = null;
+
+    $scope.date2 = null;
+    $scope.minDate2 = new Date().addDays(-7);
+    $scope.maxDate2 = new Date().addDays(7);
+    $scope.minTime2 = new Date().setHours(1, 42);
+    $scope.maxTime2 = new Date().setHours(22, 42);
+    $scope.dateFilter2 = function (date) {
+        var d = date.getDate();
+        return d % 2 !== 0 && d % 3 !== 0;
+    };
+
 }]);
