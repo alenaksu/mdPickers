fdcd56f27ddfb66d208a9c18e862b639fdacf134
Robin THONI <robin@rthoni.com>
2017-06-21 23:28:48 -0500

    Updated demo

--- demo/app.js
+++ demo/app.js
@@ -2,6 +2,12 @@
  * Created by Robin Thoni on 6/21/17.
  */
 
+Date.prototype.addDays = function(days) {
+    var dat = new Date(this.valueOf());
+    dat.setDate(dat.getDate() + days);
+    return dat;
+}
+
 angular.module('demo', [
     'ngMaterial',
     'mdPickers'
@@ -11,6 +17,29 @@ angular.module('demo', [
 
 }])
 
-.run([function() {
+.run(['$location', '$mdpLocale', function($location, $mdpLocale) {
+    if (location.search !== '?useDefaults') { // Quick and dirty
+        $mdpLocale.time.minTime = new Date().addDays(-2);
+        $mdpLocale.time.maxTime = new Date().addDays(2);
+        $mdpLocale.time.okLabel = "Valider";
+        $mdpLocale.time.cancelLabel = "Annuler";
+        $mdpLocale.time.timeFormat = "HH[h]mm";
+        $mdpLocale.time.noFloat = true;
+        $mdpLocale.time.openOnClick = true;
+        $mdpLocale.time.autoSwitch = true;
+        $mdpLocale.time.ampm = true;
 
+
+        $mdpLocale.date.minDate = new Date().addDays(-2);
+        $mdpLocale.date.maxDate = new Date().addDays(2);
+        $mdpLocale.date.okLabel = "Valider";
+        $mdpLocale.date.cancelLabel = "Annuler";
+        $mdpLocale.date.dateFilter = function (date) {
+            return date.getDate() % 2 !== 0;
+        };
+        $mdpLocale.date.dateFormat = "DD/MM/YYYY";
+        $mdpLocale.date.displayFormat = "ddd DD MMM";
+        $mdpLocale.date.noFloat = true;
+        $mdpLocale.date.openOnClick = true;
+    }
 }]);
