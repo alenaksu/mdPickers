eedcba21158729044526139c6af0359e0972f419
Robin THONI <robin@rthoni.com>
2017-06-21 19:23:09 -0500

    Added minimal demo; updated bower/npm devDependencies for the demo; updated README for the demo

---
+++ demo/app.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Robin Thoni on 6/21/17.
+ */
+
+angular.module('demo', [
+    'ngMaterial',
+    'mdPickers'
+])
+
+.config([function () {
+
+}])
+
+.run([function() {
+
+}]);
---
+++ demo/demoController.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Robin Thoni on 6/21/17.
+ */
+
+angular.module('demo').controller('demoController', ['$scope', function($scope) {
+    $scope.date1 = null;
+}]);
