d7281ba036f86ec26b4a6a8af50ccd35077e685b
Robin THONI <robin@rthoni.com>
2017-06-22 20:30:47 -0500

    Updated demo for time picker fix

--- demo/demoController.js
+++ demo/demoController.js
@@ -8,8 +8,8 @@ angular.module('demo').controller('demoController', ['$scope', function($scope) 
     $scope.date2 = null;
     $scope.minDate2 = new Date().addDays(-7);
     $scope.maxDate2 = new Date().addDays(7);
-    $scope.minTime2 = new Date().setHours(1, 42);
-    $scope.maxTime2 = new Date().setHours(22, 42);
+    $scope.minTime2 = "01:42";
+    $scope.maxTime2 = "23:42";
     $scope.dateFilter2 = function (date) {
         var d = date.getDate();
         return d % 2 !== 0 && d % 3 !== 0;
